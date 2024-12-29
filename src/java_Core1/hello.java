75f84c03b490420b2f0409e004e23b8c044e1a94
vo tri thuc <trithucvo12345@gmail.com>
2024-12-29 13:19:26 +0700

    ktao init

---
+++ src/java_Core1/hello.java
@@ -0,0 +1,10 @@
+package java_Core1;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Vo Tri Thuc");
+		System.out.println("Vo Tri Thuc");
+		System.out.println("Vo Tri Thuc");
+		System.out.println("Vo Tri Thuc");
+	}
+}
